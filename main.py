16e8bd2fdad94d85f1a95ce8b65b9b44f1ce5126
Fatih <turgayfatih.yasar@hotmail.com>
2024-12-20 23:37:33 +0100

    secretsss

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 API_KEY = "12345-SECRET-KEY"
-SECRET = "AIzaSyDaGmWKa4JsXZ-HjGw7ISLn_3namBGewQe" #test #test #TEST #dskjihfjksdhfjksz
+SECRET = "AIzaSyDaGmWKa4JsXZ-HjGw7ISLn_3namBGewQe" #test #test #TEST #dskjihfjksdhfjksz @DESLKJHFSKJLHFSLJKHDFSJKLHDFKSJ
