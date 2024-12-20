6838af524b0c274caeea2919df98e84ae4c48e5d
Fatih <turgayfatih.yasar@hotmail.com>
2024-12-20 23:34:27 +0100

    secrets

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-# secret.py
-api_key = "12345-SECRET-KEY"
-SECRET = "AIzaSyDaGmWKa4JsXZ-HjGw7ISLn_3namBGewQe" #test #test
+API_KEY = "12345-SECRET-KEY"
+SECRET = "AIzaSyDaGmWKa4JsXZ-HjGw7ISLn_3namBGewQe" #test #test #TEST #dskjihfjksdhfjksz
